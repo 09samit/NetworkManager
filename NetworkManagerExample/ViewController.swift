07e68eafbe617ec1194d111f8f6e30452e927236
Amit Garg <09s.amitgarg@gmail.com>
2022-11-30 00:10:58 +0530

    Add example

---
+++ NetworkManagerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkManagerExample
+//
+//  Created by Amit Garg on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
